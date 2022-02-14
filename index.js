1f804a7fc2d67b834df3e1be9c390325df8208d4
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-02-14 12:50:10 -0800

    added questions and prompts for user input on init

--- index.js
+++ index.js
@@ -4,13 +4,33 @@ const fs = require("fs");
 const { type } = require("os");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Enter project title.", "Enter a description for project."];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(`${data.title}.md`, generateMarkdown(data))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const promptUser = () => {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: questions[0],
+            },
+            {
+                type: "input",
+                name: "description",
+                message: questions[1],
+            },
+        ])
+    };
+
+    promptUser()
+        .then((answers) => writeToFile(answers))
+}
 
 // Function call to initialize app
 init();
